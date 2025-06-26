13ab691e57bd5fedf2fa1af478752d18e974cd48
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2025-06-26 22:34:30 +0800

    header component

--- elago-portfolio/app/page.js
+++ elago-portfolio/app/page.js
@@ -1,9 +1,11 @@
 'use client';
 import Navbar from "./components/navbar.jsx";
+import Header from "./components/header.jsx";
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 }
